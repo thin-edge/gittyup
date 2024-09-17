881cfdc879209407d28753f8b98fa22a2accf803
Reuben Miller <reuben.d.miller@gmail.com>
2024-09-17 14:43:26 +0200

    move mqtt client to gittyup client

--- gittyup.py
+++ gittyup.py
@@ -4,9 +4,87 @@ import json
 import os
 import time
 import tomllib
+import sys
 from pathlib import Path
 import git
 from git.exc import GitCommandError
+import paho.mqtt.client as mqtt
+
+
+class GittyUpClient():
+    """ GittyUp client 
+    
+    The GittyUp client is used to communicate with thin-edge via MQTT
+    """
+    def __init__(self):
+        self.client = None
+        self.device_id = 'main'
+        self.root = 'te/device/main//'
+        self.sub_topic = f'{self.root}/cmd/device_profile/+'
+        
+    def connect(self):
+        """Connect to the thin-edge.io MQTT broker"""
+        if self.client is not None:
+            print(f"MQTT client already exists. connected={self.client.is_connected()}")
+            return
+        
+        # Don't use a clean session so no messages will go missing
+        client = mqtt.Client(client_id=self.device_id, clean_session=False)
+        client.reconnect_delay_set(10, 120)
+
+        client.on_connect = self.on_connect
+        client.on_disconnect = self.on_disconnect
+        client.on_message = self.on_message
+        client.on_subscribe = self.on_subscribe
+
+        print(f"Trying to connect to the MQTT broker: host=localhost:1883")
+            
+        client.connect("localhost", 1883)
+        client.loop_start()
+
+        self.client = client
+
+    def shutdown(self):
+        """Shutdown client including any workers in progress"""
+        if self.client and self.client.is_connected():
+            self.client.disconnect()
+            self.client.loop_stop()
+
+    def subscribe(self):
+        """Subscribe to thin-edge.io device profile topic."""
+        self.client.subscribe(self.sub_topic)
+        print(f"subscribed to topic {self.sub_topic}")
+
+    def loop_forever(self):
+         """Block infinitely"""
+         self.client.loop_forever()
+
+    def publish_tedge_command(self, topic, payload):
+        """ Create tedge command"""
+
+        self.client.publish(topic=topic, payload=payload, retain=True, qos=1)
+
+    def on_connect(self, client, userdata, flags, reason_code):
+            if reason_code != 0:
+                print(f"Failed to connect. result_code={reason_code}. Retrying the connection")
+            else:
+                print(f"Connected to MQTT broker! result_code={reason_code}")
+                self.subscribe()
+
+    def on_disconnect(self, client, userdata, reason_code):
+        print(f"Client was disconnected: result_code={reason_code}")
+
+    def on_message(self, client, userdata, message):
+        payload_dict = json.loads(message.payload)
+
+        if payload_dict['status'] == "successful":
+            self.publish_tedge_command(message.topic, '')
+            
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        for sub_result in granted_qos:
+            if sub_result == 0x80:
+                # error processing
+                print(f"Could not subscribe to {self.sub_topic}. result_code={granted_qos}")
 
 
 def read_repo_url_from_toml(config_file: str) -> str:
@@ -100,5 +178,20 @@ def clone_or_pull_repo(repo_url, clone_dir="repo") -> bool:
 
 if __name__ == "__main__":
     repo_url = read_repo_url_from_toml("config.toml")
-    # clone_or_pull_repo(repo_url)
-    wait_for_updates(repo_url)
+    client = GittyUpClient()
+    try:
+        client.connect()
+        # Wait for connection
+        time.sleep(1)
+
+        # Note: wait for update is a forever loop
+        wait_for_updates(repo_url)
+    except ConnectionRefusedError:
+            print("MQTT broker is not ready yet")
+    except KeyboardInterrupt:
+            print("Exiting...")
+            if client:
+                client.shutdown()
+            sys.exit(0)
+    except Exception as ex:
+            print("Unexpected error. %s", ex)
